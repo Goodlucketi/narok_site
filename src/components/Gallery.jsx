f0d0197d9336eae92560c99f4f7a8703ff6b1e87
Goodlucketi <goodlucketim94@gmail.com>
2026-01-31 22:00:25 +0100

    components-update-lazy loading

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -40,11 +40,11 @@ import img37 from "../assets/gallery/gallery37.jpg";
 import img38 from "../assets/gallery/gallery38.jpg";
 
 const rowOneImages = [
-  img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img23, img24, img25, img26, img27, img28, img29, img30,
+  img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img23, img24, img25, img26, img27, img28, img29, img38,
 ];
 
 const rowTwoImages = [
-  img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img32, img33, img34, img35, img36, img37, img38, img31,
+  img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img32, img33, img34, img35, img36, img37, img30, img31,
 ];
 
 const Gallery = () => {
